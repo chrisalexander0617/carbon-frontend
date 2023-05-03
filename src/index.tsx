dd4fa6bce2cba8f6927eba06570b9e8a4b44be1a
Chris <chrisalexander0617@gmail.com>
2023-05-03 17:52:23 -0400

    Updated to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-// const container = document.getElementById('root')!;
-// const root = createRoot(container);
-const container = document.getElementById('root');
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
-if (container) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    container
-  );
-}

5038f5520d9caa7134ce575de40d914caad139bd
Chris <chrisalexander0617@gmail.com>
2023-05-03 12:51:58 -0400

    Cleaned component

--- src/components/Charts/BarChart.tsx
+++ src/components/Charts/BarChart.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect, useRef } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -23,7 +22,6 @@ ChartJS.register(
 );
 
 const BarChart = (props: { label: IMethaneData[], category: string }) => {
-
   const { label, category } = props;
 
   const mappedDataForLabelsByDate = label.map((item) => (
@@ -33,9 +31,6 @@ const BarChart = (props: { label: IMethaneData[], category: string }) => {
   const averages = label.map((item) => (
     item.value.max
   ))
-
-  const [chartDimensions, setChartDimensions] = useState({ width: 0, height: 0 });
-  const chartRef = useRef<any>();
 
   const options = {
     responsive: true,
@@ -49,7 +44,6 @@ const BarChart = (props: { label: IMethaneData[], category: string }) => {
         display: false,
       },
     },
-
   };
 
   const data = {
@@ -63,25 +57,9 @@ const BarChart = (props: { label: IMethaneData[], category: string }) => {
     ],
   };
 
-  useEffect(() => {
-    const chart = chartRef.current?.chartInstance;
-    if (chart) {
-      const updateChartDimensions = () => {
-        const width = chartRef.current.offsetWidth;
-        const height = chartRef.current.offsetHeight;
-        setChartDimensions({ width, height });
-      };
-      window.addEventListener('resize', updateChartDimensions);
-      return () => window.removeEventListener('resize', updateChartDimensions);
-    }
-  }, [chartRef]);
-
-
   return (
     <Bar id="bar-chart" data-testid="bar-chart" options={options}
       data={data}
-      width={chartDimensions.width}
-      height={chartDimensions.height}
     />
   );
 }
--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -1,16 +1,14 @@
-import { useState, useEffect, useRef } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement,
   PointElement,
   LineElement,
   Title,
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Bar, Line } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { IMethaneData } from '../../types/methane';
 import { convertToReadableDateFormat } from '../../utils';
 import { theme } from "../../../src/app/theme";
@@ -39,12 +37,7 @@ export const options = {
   },
 };
 
-
 const LineChart = (props: { label: IMethaneData[], category: string }) => {
-
-  const [chartDimensions, setChartDimensions] = useState({ width: 0, height: 0 });
-  const chartRef = useRef<any>();
-
   const { label, category } = props;
 
   const mappedDataForLabelsByDate = label.map((item) => (
@@ -66,19 +59,6 @@ const LineChart = (props: { label: IMethaneData[], category: string }) => {
       },
     ],
   };
-
-  useEffect(() => {
-    const chart = chartRef.current?.chartInstance;
-    if (chart) {
-      const updateChartDimensions = () => {
-        const width = chartRef.current.offsetWidth;
-        const height = chartRef.current.offsetHeight;
-        setChartDimensions({ width, height });
-      };
-      window.addEventListener('resize', updateChartDimensions);
-      return () => window.removeEventListener('resize', updateChartDimensions);
-    }
-  }, [chartRef]);
 
 
   return <Line id="line-chart" data-testid="line-chart" options={options} data={data} />;
